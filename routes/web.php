58493a94c4bc8f7413685fde403b947102d24a27
DannaCarrillo <dcarrillo882@unab.edu.co>
2025-09-04 10:56:54 -0500

    variables y tipos de variables

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "*****VARIBALE SY TIPOS DE VARIABLES***** <br><br>";
+    $name="Danna"; //string
+    $age=20; //integer
+    $height=1.60; //float
+    $isStudent=true; //boolean
+    $fruits=["manzana","pera","uva"]; //array
+
+    echo "Mi nombre es: $name, tengo $age años, mido $height metros, es $isStudent que soy estudiante y mis frutas favoritas son: ";
+    foreach($fruits as $fruit){
+        echo "$fruit ";
+    }
 });
